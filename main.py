4ca59fa5ded1227d96a3ac272923c7fc8d271a01
daisy1cutter9 <mrachunkova1@gmail.comgit config --global user.namedaisy1cutter9>
2023-04-10 13:50:42 +0200

    Prvni commit, pridala jsem basic_calculator

---
+++ main.py
@@ -0,0 +1,23 @@
+n1 = float(input("What is first number, Filipe: "))
+op = input("Enter operator (+, -, /, *, %): ")
+n2 = float(input("What is second number, Filipe: "))
+
+if op == "+":
+    result = n1 + n2
+    print(round(result, 3))
+elif op == "-":
+    result = n1 - n2
+    print(round(result, 3))
+elif op == "/":
+    result = n1 / n2
+    print(round(result, 3))
+elif op == "*":
+    result = n1 * n2
+    print(round(result, 3))
+elif op == "%":
+    result = n1 % n2
+    print(round(result, 3))
+
+else:
+    print(f"{op} is not valid input")
+
